bdc8279c1e979c006429da1bcb9af4438446cf99
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 12:18:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,10 @@
 
 function forLoop(array) {
 for (let i = 0; i < 25; i += 1) {
-  if i === 1 {
-  array.push("I am strange loop" ${`i`})
+  if(i === 1) {
+  array.push("I am 1 strange loop")
   } else {
-  "I am 1 strange loop"
-  "I am ${i} strange loops."
+  array.push("I am ${i} strange loops.")
   }
 }
 }
