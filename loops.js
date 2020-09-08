0a83a8f55fbac49069a4c4e9d74191cbb15fee5d
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 12:32:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,9 +4,11 @@ for (let i = 0; i < 25; i ++) {
   array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
 }
 console.log(array)
-return array
+  return array
 }
 
 function whileLoop(n) {
-  while
+  while (n < 10) {
+    
+  }
 }
